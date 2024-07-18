59bfd6362abe17a77c0b6264c909a9b23dec8d92
siyoon <ryuu.public@gmail.com>
2024-07-18 21:38:26 +0900

    feat: add websocket test api with dto

---
+++ src/main/java/com/green/bamboo/api/greeting/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.green.bamboo.api.greeting.controller;
+
+import com.green.bamboo.api.greeting.dto.Greeting;
+import com.green.bamboo.api.greeting.dto.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
